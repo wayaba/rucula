d13d854aa1c6f38052746f514ea1d6b03377ca1d
Pablo Pedraza <wensel84@gmail.com>
2023-05-08 09:59:31 -0300

    edito name

--- src/app/components/Cotization.jsx
+++ src/app/components/Cotization.jsx
@@ -1,10 +1,13 @@
+const formatCotizationName = (name) => {
+  return name.replace('Dolar', '').trim()
+}
 export function Cotization({ cotization }) {
   return (
     <section className=" bg-emerald-800 rounded-xl p-4">
       <ul className="flex flex-col gap-4 text-white">
         {cotization.map(({ name, buy, sell }) => (
           <li key={name} className="flex justify-between gap-4 items-end">
-            <div className=" font-bold">{name}</div>
+            <div className=" font-bold">{formatCotizationName(name)}</div>
             <div className="flex items-center gap-4">
               <div className="text-xl font-bold ">
                 <div className="text-sm opacity-50">Compra</div>
--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header({ dateTime }) {
   return (
     <header className="text-center m-3">
-      <h1 className="text-4xl">Cotización Dolar 💸</h1>
+      <h1 className="text-4xl">Cotización Dólar 💸</h1>
       <small className="opacity-50">{dateTime}</small>
     </header>
   )
