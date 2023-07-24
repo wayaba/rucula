0276b9b68325f113b7fb375e89e709b0a9b7a3b2
Pablo Pedraza <wensel84@gmail.com>
2023-07-24 09:02:38 -0300

    agrego body component

--- src/app/components/Body.jsx
+++ src/app/components/Body.jsx
@@ -6,70 +6,68 @@ import { ReloadIcon } from '../components/Icons'
 import { getCotization } from '../services/dolarSi'
 import { Spinner } from '../components/Spinner'
 
+export function Body({ cotization }) {
+  const [amount, setAmount] = useState(0)
+  const [idSelected, setIdSelected] = useState(1)
+  const [currencySelected, setCurrencySelected] = useState('pesos')
+  const [currentCotization, setCurrentCotization] = useState(cotization)
+  const [loading, setLoading] = useState(false)
+  const [currentDateTime, setCurrentDateTime] = useState('23/5/2023, 9:56:11')
 
-export function Body({ cotization }){
-    const [amount, setAmount] = useState(0)
-    const [idSelected, setIdSelected] = useState(1)
-    const [currencySelected, setCurrencySelected] = useState('pesos')
-    const [currentCotization, setCurrentCotization] = useState(cotization)
-    const [loading, setLoading] = useState(false)
-    const [currentDateTime, setCurrentDateTime] = useState('23/5/2023, 9:56:11')
+  useEffect(
+    () => setCurrentDateTime(new Date(cotization.dateTime).toLocaleString()),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  )
 
+  const handleChange = (value) => {
+    setIdSelected(value)
+  }
+  const handleChangeCurrency = (value) => {
+    setCurrencySelected(value)
+  }
 
-    useEffect(
-        () => setCurrentDateTime(new Date(cotization.dateTime).toLocaleString()),
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        []
-      )
+  const handleChangeAmount = (value) => {
+    setAmount(value)
+  }
 
-    const handleChange = (value) => {
-        setIdSelected(value)
-      }
-      const handleChangeCurrency = (value) => {
-        setCurrencySelected(value)
-      }
-    
-      const handleChangeAmount = (value) => {
-        setAmount(value)
-      }
-    
-      const handleClickUpdate = async () => {
-        setLoading(true)
-        const newCotization = await getCotization()
-        setCurrentCotization(() => newCotization)
-        setCurrentDateTime(new Date().toLocaleString())
-        setLoading(false)
-      }
+  const handleClickUpdate = async () => {
+    setLoading(true)
+    const newCotization = await getCotization()
+    setCurrentCotization(() => newCotization)
+    setCurrentDateTime(new Date().toLocaleString())
+    setLoading(false)
+  }
 
-    return (
-        <section className="bg-gray-600 text-white shadow-md rounded-xl p-2 m-2">
-            {loading && <Spinner />}
-        <div className="flex flex-1 text-center m-1 justify-center gap-2">
-          <button
-            className="flex items-center cursor-pointer hover:opacity-70"
-            title="Actualizar"
-            onClick={handleClickUpdate}
-          >
-            <small className="opacity-50 text-center mr-2">
-              {currentDateTime}
-            </small>
-            <ReloadIcon />
-          </button>
-        </div>
+  return (
+    <section className="bg-gray-600 text-white shadow-md rounded-xl p-2">
+      {loading && <Spinner />}
+      <div className="flex flex-1 text-center m-1 justify-center gap-2">
+        <button
+          className="flex items-center cursor-pointer hover:opacity-70"
+          title="Actualizar"
+          onClick={handleClickUpdate}
+        >
+          <small className="opacity-50 text-center mr-2">
+            {currentDateTime}
+          </small>
+          <ReloadIcon />
+        </button>
+      </div>
 
-        <Cotization
-          cotization={currentCotization.items}
-          idSelected={idSelected}
-          onChange={(value) => handleChange(value)}
-        />
-        <Calculator
-          cotization={currentCotization.items}
-          idSelected={idSelected}
-          currencySelected={currencySelected}
-          amount={amount}
-          onChange={(value) => handleChangeAmount(value)}
-          handleChangeCurrency={(value) => handleChangeCurrency(value)}
-        />
-      </section>
-    )
+      <Cotization
+        cotization={currentCotization.items}
+        idSelected={idSelected}
+        onChange={(value) => handleChange(value)}
+      />
+      <Calculator
+        cotization={currentCotization.items}
+        idSelected={idSelected}
+        currencySelected={currencySelected}
+        amount={amount}
+        onChange={(value) => handleChangeAmount(value)}
+        handleChangeCurrency={(value) => handleChangeCurrency(value)}
+      />
+    </section>
+  )
 }
--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ import Cafecito from "./Cafecito";
 
 export function Footer({ author, github }) {
   return (
-    <footer className="flex flex-col text-xs bottom-0 left-0 right-0 2xl:mt-10 2xl:pb-20 lg:mt-3  md:mt-3 md:pb-2 mt-10 pb-5 text-center opacity-60  text-white/80 justify-center">
+    <footer className="flex flex-col text-xs bottom-0 left-0 right-0 2xl:pb-20 md:pb-2 pb-5 text-center opacity-60  text-white/80 justify-center">
       <div>
       {' '}
       Desarrollado por{' '}
